157a82b0f49019cd78e0c3e476d727e87f2e43dc
Kene <kayyungx@gmail.com>
2022-11-12 21:00:18 +0100

    Add collection component

---
+++ src/components/Furnitures/FurnitureSummary.jsx
@@ -0,0 +1,18 @@
+const FurnitureSumarry = () => {
+  return (
+    <div>
+      <h3>World Class Furniture, Delivered to you</h3>
+      <p>
+        Choose your favourite set of furnitures from our catalogs that contians
+        furnitures that would suit your creativity
+      </p>
+      <p>
+        All our furnitures are crafted by world class crafstmen, with high
+        quality, world class materials. We take pride in our products cause they
+        always stand out
+      </p>
+    </div>
+  );
+};
+
+export default FurnitureSumarry;
