30f6edb60849ea9d2f9f3036dc8b5560c4c7dc4e
Kene <kayyungx@gmail.com>
2022-11-13 20:31:37 +0100

    Add products section

--- src/components/Furnitures/AvailableFurnitures.jsx
+++ src/components/Furnitures/AvailableFurnitures.jsx
@@ -1,42 +1,54 @@
 import { v4 as uuidv4 } from 'uuid';
 import icons from '../../images/icons.svg';
+import product1 from '../../images/product-1.png';
+import product2 from '../../images/product-2.png';
+import product3 from '../../images/product-3.png';
+import product4 from '../../images/product-4.png';
+import product5 from '../../images/product-5.png';
+import product6 from '../../images/product-6.png';
 
 const DUMMY_FURNITURES = [
   {
     id: uuidv4(),
     name: 'Ramen Stool',
     price: '$65.00',
-    formerPrice: '$95.00'
+    formerPrice: '$95.00',
+    productImage: product1
   },
   {
     id: uuidv4(),
     name: 'Flexform',
     price: '$35.00',
-    formerPrice: '$55.00'
+    formerPrice: '$55.00',
+    productImage: product2
   },
   {
     id: uuidv4(),
     name: 'Porada',
     price: '$87.00',
-    formerPrice: '$97.00'
+    formerPrice: '$97.00',
+    productImage: product3
   },
   {
     id: uuidv4(),
     name: 'Amora',
     price: '$65.00',
-    formerPrice: '$95.00'
+    formerPrice: '$95.00',
+    productImage: product4
   },
   {
     id: uuidv4(),
     name: 'Taknion',
     price: '$45.00',
-    formerPrice: '$55.00'
+    formerPrice: '$55.00',
+    productImage: product5
   },
   {
     id: uuidv4(),
     name: 'Liquid Contract',
     price: '$35.00',
-    formerPrice: '$45.00'
+    formerPrice: '$45.00',
+    productImage: product6
   }
 ];
 
@@ -60,7 +72,9 @@ const AvailableFurnitures = () => {
           <p className="mr-8">{furniture.price}</p>
           <p className="text-slate-300 line-through">{furniture.formerPrice}</p>
         </div>
-        <div></div>
+      </div>
+      <div className="flex justify-center items-center">
+        <img src={furniture.productImage} alt="" className="h-48" />
       </div>
     </li>
   ));
--- src/components/Layout/Products.jsx
+++ src/components/Layout/Products.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import AvailableFurnitures from '../Furnitures/AvailableFurnitures';
 
 const Products = () => {
@@ -12,14 +13,14 @@ const Products = () => {
         <h3 className="text-4xl mr-16">PRODUCTS</h3>
         <p className="mt-8 mr-16 text-slate-400">Best Selling</p>
         <p className="mt-8 mr-64 text-slate-400">Most Popular</p>
-        <a href="" className="mt-8 mr-16 text-xs">
+        <a href="/" className="mt-8 mr-16 text-xs">
           See All
         </a>
       </div>
       <div className="mt-16">
         <ul className="flex items-center justify-center text-xs">
           {links.map((link) => (
-            <li className="mr-6 text-slate-400">
+            <li className="mr-6 text-slate-400" key={uuidv4()}>
               <a
                 href={`#${link}`}
                 className={`${active === link && 'text-black'}`}
@@ -32,7 +33,7 @@ const Products = () => {
         </ul>
       </div>
       <div>
-        <AvailableFurnitures></AvailableFurnitures>
+        <AvailableFurnitures />
       </div>
     </section>
   );
